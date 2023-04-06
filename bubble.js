79ea65b6aec23a1f00286027efc9ad6912858ecd
gamountainmama <gamountainmama@gmail.com>
2023-04-05 23:00:28 -0500

    updated bubble.js and index.html

--- bubble.js
+++ bubble.js
@@ -1,14 +1,19 @@
 var data = d3.csv("Resources/atl_data.csv").then(function(data) {
     var meanTravel = [];
     for (i=0;i<data.length;i++) {
-    meanTravel.push(data[i]["2016-2020 Mean Travel Time in Minutes"])
+        meanTravel.push(data[i]["2016-2020 Mean Travel Time in Minutes"])
     }
     console.log(meanTravel);
     medIncome = [];
     for (i=0;i<data.length;i++) {
-    medIncome.push(data[i]["2020 Model-Based Estimate of Median Household Income, Dollars"])
+        medIncome.push(data[i]["2020 Model-Based Estimate of Median Household Income, Dollars"])
     }
     console.log(medIncome);
+    countyList = [];
+    for (i=0;i<data.lenth;i++) {
+        countyList.push(data[i].County)
+    console.log(countyList)
+    }
 
     // define the data for the bubble chart
     var bubble = [{
@@ -20,12 +25,22 @@ var data = d3.csv("Resources/atl_data.csv").then(function(data) {
     
     var bubbleLayout = {
         xaxis: {title: 'Median Income'},
-        yaxis: {title: 'Mean Commute Time'}
+        yaxis: {title: 'Mean Commute Time'},
+        text: 'countyList'
         };
     
     // plot the bubble chart
     Plotly.newPlot('bubble', bubble, bubbleLayout);
 
-var countySelect = = document.getElementById('selCounty');
-if 
 });
+
+var countySelect = document.getElementById('selCounty');
+
+var bubbleColor = [{
+    x: medIncome,
+    y: meanTravel,
+    mode: 'markers',
+    marker: {size: meanTravel, color: countyColors(countySelect)},
+    }];
+
+Plotly.updatePlot('bubble', bubble, bubbleLayout)
